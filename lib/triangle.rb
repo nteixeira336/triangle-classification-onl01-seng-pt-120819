a8fcfadd6341cdeaa4473902ad93a165f114359d
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-02-03 16:19:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
-class Triangle
-  # write code here
+class Triangle 
+  
+  attr_accessor :a, :b, :c 
+  
+  def initialize(a,b,c)
+    @a=a 
+    @b=b 
+    @c=c 
+  end 
+  
+  def kind 
+    
+  end 
 end
